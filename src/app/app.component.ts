3eed22909cfd421f08e71e3e306c2798141169ae
EdsonAmaya7 <edson_amaya99@live.com>
2023-02-08 10:30:46 -0600

    primera seccion finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'FerNanDo'
-
-  mostrarNombre(){
-    console.log(this.nombre)
+  
+  constructor(private primengConfig: PrimeNGConfig) {}
+  
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
+  //i18nSelect
+  nombre: string = "Susana";
+  genero: string = "femenino";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla',
   }
 
+  cambiarPersona() {
+    if (this.nombre == 'Susana') {
+      this.nombre = 'Fernendo';
+      this.genero = 'masculino'
+      return;
+    }
+    this.nombre = 'Susana';
+    this.genero = 'femenino'
+  }
+
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Jose', 'mario', 'fernando'];
+  clientesMapa = {
+    '=0': 'No tenemos ningún cliente esperando.',
+    '=1': 'Tenemos un cliente esperando.',
+    'other': 'Tenemos # clientes esperando'
+  }
+
+  borrarPersona() {
+    this.clientes.pop();
+  }
+
+  // keyvalue pipe
+  persona = {
+    nombre: 'Fernando',
+    edad: 35,
+    direccion: 'ottawa, Canadá'
+  }
+
+  // jsonPipe
+  heroes = [
+    {
+      nombre: 'superman',
+      vuela: true
+    },
+    {
+      nombre: 'robin',
+      vuela: false
+    },
+    {
+      nombre: 'aquaman',
+      vuela: false
+    }
+  ]
+
+  // async pipe
+  miObservable = interval(5000);
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+    
+  });
 }
